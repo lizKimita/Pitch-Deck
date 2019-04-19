e70911e25211b88ea368c20c7c015737bd0975bf
lizKimita <kimita.wanjiku@gmail.com>
2019-04-19 17:27:44 +0300

    adding the error page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from app import DevConfig
+from flask_bootstrap import Bootstrap
+from app import views
+from app import error
 
 # Initializing application
 app = Flask(__name__)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
+
+# Initializing Flask Extensions
+bootstrap = Bootstrap(app)
 
 from app import views
