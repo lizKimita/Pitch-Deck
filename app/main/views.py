fd5834e78df823719357cfb40914e4819dc2800a
lizKimita <kimita.wanjiku@gmail.com>
2019-04-20 08:34:25 +0300

    linking the home page with the pitches display page

--- app/main/views.py
+++ app/main/views.py
@@ -14,31 +14,31 @@ def index():
     title = "Make the right first impression"
     return render_template('index.html', title = title)
 
-@main.route('/category/<name>')
-def category(name):
+@main.route('/categories/')
+def categories():
 
     '''
     View categories page function that returns the categories details page and its data
     '''
 
-    category = get_category(name)
-    pitches = Pitch.get_pitches(category.name)
+    # category = get_category(name)
+    pitches = Pitch.get_pitches(categories)
 
-    return render_template('categories.html',name = name, category = category, pitches = pitches)
+    return render_template('categories.html', pitches = pitches)
 
-@main.route('/category/pitch/new/<name>', methods = ['GET','POST'])
-def new_pitch(name):
-    form = PitchForm()
-    category = get_category(name)
+# @main.route('/category/pitch/new/<name>', methods = ['GET','POST'])
+# def new_pitch(name):
+#     form = PitchForm()
+#     # category = get_category(name)
 
-    if form.validate_on_submit():
-        pitch = form.pitch.data
-        postedOn = form.date.data
-        new_pitch = Pitch(pitch, postedOn)
-        new_pitch.save_pitch()
-        return redirect(url_for('.category',name = category.name ))
+#     if form.validate_on_submit():
+#         pitch = form.pitch.data
+#         postedOn = form.date.data
+#         new_pitch = Pitch(pitch, postedOn)
+#         new_pitch.save_pitch()
+#         return redirect(url_for('.category',name = category.name ))
 
-    title = f'{category.name} pitch'
-    return render_template('new_pitch.html',title = title, pitch_form = form, category=category)
+#     title = f'{category.name} pitch'
+#     return render_template('new_pitch.html',title = title, pitch_form = form, category=category)
 
     # self, author, category_name, pitch, PostedOn, upVote, downVote
